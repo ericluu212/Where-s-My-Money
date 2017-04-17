924b2dde87f26fb93ff1c41294d168fbb202085f
mtmcgraw <mtmcgraw@mit.edu>
2017-04-17 16:32:50 -0400

    Added filtering

--- transaction.js
+++ transaction.js
@@ -1,8 +1,9 @@
-var Transaction = function(date, description, cost, id)
+var Transaction = function(date, description, cost, account, id)
 {
 	Object.defineProperty(this, 'id', {value: id, writable: false});
 
 	this.date = date;
 	this.description = description;
 	this.cost = cost;
+	this.account = account;
 }
