3ef7c180c960183844e438d5531170bad16eb17b
amontane <andrew@dhcp-18-111-106-61.dyn.MIT.EDU>
2017-04-16 22:02:00 -0400

    transfer added

--- transferModal.js
+++ transferModal.js
@@ -1,17 +1,12 @@
-var Transfer = function(){
+function transfer(){
 	// Get the modal
 	var modal = document.getElementById('transferModal');
-
-	// Get the button that opens the modal
-	var btn = document.getElementById("transfer-button");
 
 	// Get the <span> element that closes the modal
 	var span = document.getElementsByClassName("close")[0];
 
 	// When the user clicks the button, open the modal 
-	btn.onclick = function() {
-	    modal.style.display = "block";
-	}
+	modal.style.display = "block";
 
 	// When the user clicks on <span> (x), close the modal
 	span.onclick = function() {
